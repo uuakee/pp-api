cef6a7a71445cc9ca9af4e7ba8ee7c3ea4a398a7
joao-gallo <contato@kaueguedes.com.br>
2025-02-12 01:18:46 -0300

    update reset password route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, loginUser, getUser, getUsers, getBalance, buyPlan, getPlansFromUser, getDeposits, getWithdrawals } = require('../controllers/user.controller');
+const { createUser, loginUser, getUser, getUsers, getBalance, buyPlan, getPlansFromUser, getDeposits, getWithdrawals, updateUser } = require('../controllers/user.controller');
 
 router.post('/register', createUser);
 router.post('/login', loginUser);
 router.get('/info/:userId', getUser);
+router.put('/update/:userId', updateUser);
 router.get('/all', getUsers);
 router.get('/balance', getBalance);
 router.post('/buy-plan', buyPlan);
